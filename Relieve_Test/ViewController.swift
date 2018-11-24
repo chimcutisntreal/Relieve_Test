00fd5e97b457b49dc988bf955cd59218029e87bc
Chinh <chinh@chinhs-macbook-pro.local>
2018-11-24 20:17:11 +0700

    Initial Commit

---
+++ Relieve_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Relieve_Test
+//
+//  Created by Chinh on 11/24/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
